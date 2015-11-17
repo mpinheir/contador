ab82801ff49166eee94f689932342a16a2f8c3fb
Marcelo Pinheiro <mpinheir@gmail.com>
2015-11-16 20:41:57 -0800

    Initial Commit

---
+++ Contador/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contador
+//
+//  Created by Marcelo Pinheiro on 2015-11-16.
+//  Copyright © 2015 Pinheiro Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
