213dcf07f1ca879f10ca741b4b2d12d3d5d0495e
Marcelo Pinheiro <mpinheir@pelware.com>
2016-09-09 22:23:23 -0700

    First Commit

--- Contador/ViewController.swift
+++ Contador/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label1: UILabel!
+    
+    var i = 0
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.Label1.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func incrementa(_ sender: AnyObject) {
+        i = i + 1
+        self.Label1.text = String(i)
+    }
 
+
+    @IBAction func decrementa(_ sender: AnyObject) {
+        if (i > 0)
+        { i -= 1
+        }
+        
+        self.Label1.text = String(i)
+    }
+
+    @IBAction func reset(_ sender: AnyObject) {
+        i = 0
+        
+        self.Label1.text = String(i)
+        
+    }
 }
 
